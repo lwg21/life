db75e9603574feb9d80238c7b6345a3d5f57660e
Lucas Gruner <lucas.gruner@gmail.com>
2025-02-10 10:29:36 +0100

    improve seeds with user and random habit_logs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
 HabitLog.destroy_all
 Habit.destroy_all
+User.destroy_all
 
-Habit.create(name: 'Routine')
-Habit.create(name: 'Meditation')
+puts "Creating users…"
+user = User.create(email_address: "m@m.com", password: "123456")
+
+puts "Creating habits…"
+habit1 = Habit.create(name: 'Routine', user: user)
+habit2 = Habit.create(name: 'Meditation', user: user)
+habit3 = Habit.create(name: 'Yoga', user: user)
+
+puts "Logging habits…"
+today = Date.today
+(-30..0).each do |n|
+  habit1.habit_logs.create(log_date: today + n) if rand(100) < 70
+  habit2.habit_logs.create(log_date: today + n) if rand(100) < 40
+  habit3.habit_logs.create(log_date: today + n) if rand(100) < 50
+end
+
+puts "Seeding done!"
