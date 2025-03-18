9b93d8b4f0cf71243a0220b1e761c22deb43ff79
Lucas Gruner <lucas.gruner@gmail.com>
2025-03-18 16:34:34 +0100

    user associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  has_many :habits
+  has_many :habits, dependent: :destroy
   has_many :habit_logs, through: :habits
+
+  validates :email_address, uniqueness: true
+  # validates :password, confirmation: true
+  # validates :password_confirmation, presence: true
 end
