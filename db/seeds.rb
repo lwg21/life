92304bee1ddc3c64004df5011ddde2d992e6626a
Lucas Gruner <lucas.gruner@gmail.com>
2025-03-24 12:55:00 +0100

    seed more habit logs, add streak to habit show

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,17 @@ puts "Creating habits…"
 habit1 = Habit.create(name: 'Routine', user: user)
 habit2 = Habit.create(name: 'Meditation', user: user)
 habit3 = Habit.create(name: 'Yoga', user: user)
+habit4 = Habit.create(name: 'Stretching', user: user)
+habit5 = Habit.create(name: 'Git commit', user: user)
 
 puts "Logging habits…"
 today = Date.today
-(-30..0).each do |n|
+(-300..0).each do |n|
   habit1.habit_logs.create(log_date: today + n) if rand(100) < 70
   habit2.habit_logs.create(log_date: today + n) if rand(100) < 40
   habit3.habit_logs.create(log_date: today + n) if rand(100) < 50
+  habit4.habit_logs.create(log_date: today + n) if rand(100) < 15
+  habit5.habit_logs.create(log_date: today + n) if rand(100) < 30
 end
 
 puts "Seeding done!"
