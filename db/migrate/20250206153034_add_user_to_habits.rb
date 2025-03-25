46f51fa57a9f0f5e20ba75c082e0fbcb5ca2ee00
Lucas Gruner <lucas.gruner@gmail.com>
2025-03-25 11:04:05 +0100

    remove data migration from schema migration

--- db/migrate/20250206153034_add_user_to_habits.rb
+++ db/migrate/20250206153034_add_user_to_habits.rb
@@ -1,14 +1,5 @@
 class AddUserToHabits < ActiveRecord::Migration[8.0]
-  # Add user reference column, assign previous habits to default user, enforce null
-  def up
-    add_reference :habits, :user, null: true, foreign_key: true
-    default_user = User.create_with(password: "123456")
-                       .find_or_create_by!(email_address: "user@user.com")
-    Habit.update_all(user_id: default_user.id)
-    change_column_null :habits, :user_id, false
-  end
-
-  def down
-    remove_reference :habits, :user
+  def change
+    add_reference :habits, :user, null: false, foreign_key: true
   end
 end
