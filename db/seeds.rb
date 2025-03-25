ac46a03a2e87eafe36ef1a0b921e8c6bdf718caf
Lucas Gruner <lucas.gruner@gmail.com>
2025-03-25 17:56:49 +0100

    correct goals code in seeds to use snake case for symbol usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,22 @@ puts "Creating goals…"
 goal_data = [
   {
     name: "3-day streak",
-    code: "streak-day-3",
+    code: :streak_day_3,
     description: "Do a habit 3 days in a row"
   },
   {
     name: "7-day streak",
-    code: "streak-day-7",
+    code: :streak_day_7,
     description: "Do a habit 7 days in a row"
   },
   {
     name: "15-day streak",
-    code: "streak-day-15",
+    code: :streak_day_15,
     description: "Do a habit 15 days in a row"
   },
   {
     name: "Full month",
-    code: "streak-calendar-month-1",
+    code: :streak_calendar_month_1,
     description: "Do a habit every day for a calendar month"
   }
 ]
@@ -40,7 +40,7 @@ habit5 = Habit.create!(name: 'Git commit', user: user)
 
 puts "Logging habits…"
 today = Date.today
-(-300..0).each do |n|
+(-80..0).each do |n|
   habit1.habit_logs.create!(log_date: today + n) if rand(100) < 70
   habit2.habit_logs.create!(log_date: today + n) if rand(100) < 40
   habit3.habit_logs.create!(log_date: today + n) if rand(100) < 50
