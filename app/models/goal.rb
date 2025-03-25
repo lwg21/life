974f1eca7ee6c64a2d57000761a12a3dd6119ad7
Lucas Gruner <lucas.gruner@gmail.com>
2025-03-25 14:25:37 +0100

    validations for goal and unlock

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,7 @@
 class Goal < ApplicationRecord
   has_many :unlocks, dependent: :destroy
   has_many :users, through: :unlocks
+
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: true
 end
--- app/models/unlock.rb
+++ app/models/unlock.rb
@@ -1,4 +1,6 @@
 class Unlock < ApplicationRecord
   belongs_to :user
   belongs_to :goal
+
+  validates :user_id, uniqueness: { scope: :goal_id, message: "has already unlocked this goal" }
 end
